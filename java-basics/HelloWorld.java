bfc031bf05b1b1aee395af3ad15c5f469ec0ef4c
Simran Kaur <simrankr361@gmail.com>
2019-09-27 11:03:45 +0530

    27july

---
+++ java-basics/HelloWorld.java
@@ -0,0 +1,17 @@
+// outer class ya public ya default(kuch nhi likhenge)
+// camelcase in java
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+    }
+} // do not put semicolon here
+
+// static member - bina object ke call ho sakta hai
+// System - class
+// out ek object hai - uske function println call kar rahe hai
+
+
+// to compile write - javac filename.java
+// to run - java filename
